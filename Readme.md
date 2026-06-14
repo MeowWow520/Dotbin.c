888c011fd4c7b4d2a14d59968e7eee5d4f870bdf
MeowWow520 <meow114514@outlook.com>
2026-06-14 23:49:10 +0800

    Ready Set! Go!

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Dotbin.c
+
+## What's this?
+
+Dotbin.c is a `.bin` file creator tool wrote by c
---
+++ dotbin.c
@@ -0,0 +1,12 @@
+//
+// Created by MeowWow520 on 2026/6/14.
+//
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    (void)argc, (void)argv;
+    printf("Hello Dotbin.c\n");
+    return 0;
+}
