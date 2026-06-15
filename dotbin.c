bb393a80028fa64fd73662f0396bddd15fbf6181
MeowWow520 <Meow114514@outlook.com>
2026-06-15 20:41:17 +0800

    feat(): 实现自定义输出名字

--- dotbin.c
+++ dotbin.c
@@ -4,12 +4,20 @@
 
 // dotbin.c shell commands
 // -o "output.bin": output .bin file
-// -i "input.bin" : change format
+
 
 
 #include <stdio.h>
 #include <stdint.h>
-#include <limits.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+
+#define DEFAULT_OUTPUT_FILE_NAME "output.bin"
+
+int output_file_index = 0;
+char* output_file_name = DEFAULT_OUTPUT_FILE_NAME;
 
 
 
@@ -17,9 +25,34 @@ uint8_t buffer_uint8[12] = {
     0xff, 0xfe, 0xfd, 0xfc, 0xfb, 0xfa,
     0xff, 0xef, 0xdf, 0xcf, 0xbf, 0xaf
 };
-int main(int argc, char **argv) {
+
+void read_argv(const int a, char **b) {
+    for (int i = 0; i < a; i++) {
+        if (strcmp(b[i], "-o") == 0) output_file_index = i;
+    }
+};
+
+int main(const int argc, char **argv) {
     printf("Hello Dotbin.c\n");
-    FILE *fp = fopen("numbers.bin", "wb");
+    bool markers[argc];
+    for (int i = 0; i < argc; i++) {
+        markers[i] = false;
+    }
+
+
+    if (argc != 1) {
+        read_argv(argc, argv);
+    }
+    if (output_file_index != 0 && argv[output_file_index + 1] != NULL) {
+        output_file_name = argv[output_file_index + 1];
+        printf("Output file name: %s\n", output_file_name);
+    } else {
+        printf("Output file name is not provided, using default file name: %s\n", DEFAULT_OUTPUT_FILE_NAME);
+    }
+
+
+    FILE *fp;
+    fopen_s(&fp, output_file_name, "wb");
     if (fp == NULL) return 1;
     fwrite(buffer_uint8, sizeof(uint8_t), 12, fp);
     fclose(fp);
